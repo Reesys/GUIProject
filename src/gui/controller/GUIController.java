617f8e47d6396921dd68368f39e7d1787579732f
Reesys <emailjoshuareyes@gmail.com>
2018-11-20 12:27:52 -0700

    Created my classes and imported the frame and duck class.

---
+++ src/gui/controller/GUIController.java
@@ -0,0 +1,18 @@
+package gui.controller;
+
+import gui.model.Duck;
+
+public class GUIController
+{
+	private Duck myDuck;
+
+	public GUIController()
+	{
+		myDuck = new Duck();
+	}
+
+	public void start()
+	{
+
+	}
+}
