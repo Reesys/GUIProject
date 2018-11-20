2b27feb45bbb0f13184a8e0393271ce1eb446103
Reesys <emailjoshuareyes@gmail.com>
2018-11-20 13:54:36 -0700

    Added color change listener to the panel.

--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -1,12 +1,22 @@
 package gui.view;
 
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import gui.controller.GUIController;;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+
+import gui.controller.GUIController;
 
 public class FirstPanel extends JPanel
 {
 	private GUIController appController;
+	private JLabel firstLabel;
+	private JButton firstButton;
+	private SpringLayout appLayout;
 
 	public FirstPanel(GUIController appController)
 	{
@@ -14,23 +24,47 @@ public class FirstPanel extends JPanel
 
 		this.appController = appController;
 
+		firstLabel = new JLabel("Words in a line!");
+		firstButton = new JButton("Click!!");
+		appLayout = new SpringLayout();
+
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
 
+	private void changeBackground()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		this.setBackground(new Color(red, green, blue));
+	}
+
 	private void setupPanel()
 	{
-
+		this.setLayout(appLayout);
+		this.add(firstButton);
+		this.add(firstLabel);
 	}
 
 	private void setupLayout()
 	{
-
+		appLayout.putConstraint(SpringLayout.NORTH, firstLabel, 70, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, firstLabel, -90, SpringLayout.EAST, this);
+		appLayout.putConstraint(SpringLayout .WEST, firstButton, 175, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.SOUTH, firstButton, -135, SpringLayout.SOUTH, this);
 	}
 
 	private void setupListeners()
 	{
-
+		firstButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent mouseClick)
+			{
+				changeBackground();
+			}
+		});
 	}
 }
