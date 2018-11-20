5940e47157de8c171db6918c977ca6ae814c468b
Reesys <emailjoshuareyes@gmail.com>
2018-11-20 12:53:52 -0700

    Added more JPanel methods.

--- src/gui/model/Duck.java
+++ src/gui/model/Duck.java
@@ -2,5 +2,23 @@ package gui.model;
 
 public class Duck
 {
-	
+	private String sound;
+
+	public Duck()
+	{
+		this.sound = "Quack Quack";
+	}
+
+	public String getSound()
+	{
+		return sound;
+	}
+
+	@Override
+	public String toString()
+	{
+		String description = "I am a duck!";
+
+		return description;
+	}
 }
--- src/gui/view/FirstFrame.java
+++ src/gui/view/FirstFrame.java
@@ -2,18 +2,25 @@ package gui.view;
 
 import javax.swing.JFrame;
 
+import gui.controller.GUIController;
+
 public class FirstFrame extends JFrame
 {
 
-	public FirstFrame()
+	private GUIController appController;
+	private FirstPanel appPanel;
+
+	public FirstFrame(GUIController appController)
 	{
 		super();
-
+		this.appController = appController;
+		this.appPanel = new FirstPanel(appController);
 		setupFrame();
 	}
 
 	private void setupFrame()
 	{
+		this.setContentPane(appPanel);
 		this.setSize(800, 800);
 		this.setTitle("My first GUI");
 		this.setResizable(true);
